c3dd194695658a251cc9da61f747f18e34031339
novinbukannopin <novinardiany@outlook.com>
2025-01-25 15:56:12 +0700

    feat: set main.go to cmd

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,15 @@
-package cmd
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/novinbukannopin/fc-simple-forum/internal/handlers/memberships"
+)
+
+func main() {
+	r := gin.Default()
+
+	membershipHandler := memberships.Handler{r}
+	membershipHandler.RegisterRoute()
+
+	r.Run(":9999")
+}
